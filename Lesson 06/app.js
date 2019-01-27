1481b28991bac9f6840240488a46cbe395000184
mridul <mridulsagar699@gmail.com>
2019-01-27 15:52:49 +0600

    Lesson 06: Key Modifiers

---
+++ Lesson 06/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el:'#vue-app',
+    data:{
+        
+
+    },
+    methods:{
+       
+        logName:function(){
+            console.log("You entered your name.");
+        },
+
+        logAge:function(){
+            console.log("You entered your age.");
+        }
+        
+    }  
+});
