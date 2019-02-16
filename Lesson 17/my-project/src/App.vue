c6b9382c65b9d692f89add362493f294460e353d
mridul <mridulsagar699@gmail.com>
2019-02-16 17:41:13 +0600

    Lesson 17: Vue webpack project exploring.

---
+++ Lesson 17/my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1> {{ title }} </h1>
+    <p> {{ greeting() }}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: "My first vue file..."
+    }
+  },
+  methods: {
+
+    greeting: function(){
+      return "Hello... everyone.. ";
+    }
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
