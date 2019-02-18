8e03664b695e171b9c73fbb3d7d84b7038d04e7e
mridul <mridulsagar699@gmail.com>
2019-02-18 18:09:09 +0600

    Lesson 21: Props

--- Lesson 20/my-project/src/App.vue
+++ Lesson 20/my-project/src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <div>
     <app-header></app-header>
-    <app-ninjas></app-ninjas>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
     <app-footer></app-footer>
   </div>
-
 </template>
 
 <script>
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Ninjas from './components/Ninjas';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Ninjas from "./components/Ninjas";
 
 export default {
-
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
   },
   data() {
-    return {};
+    return {
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data Diggin", show: false }
+      ]
+    };
   }
 };
 </script>
