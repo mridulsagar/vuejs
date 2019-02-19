3d0dd662486d4a60e823a043dfb932105d227926
mridul <mridulsagar699@gmail.com>
2019-02-19 12:48:07 +0600

    Lesson 27: Input Binding (Creating a blog, part 1)

---
+++ Lesson 27/my-blog/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <add-blog></add-blog>
+</template>
+
+<script>
+import addBlog from "./components/AddBlog.vue";
+export default {
+  components: {
+    "add-blog": addBlog
+  },
+
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
