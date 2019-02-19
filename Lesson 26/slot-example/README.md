b5019897720dc365900c6e6f5ab33524cd9326ea
mridul <mridulsagar699@gmail.com>
2019-02-19 09:13:38 +0600

    Lesson 26: Dynamic Components

---
+++ Lesson 26/slot-example/README.md
@@ -0,0 +1,33 @@
+# Lesson 26: Dynamic Components
+[keep-alive with Dynamic Components](https://vuejs.org/v2/guide/components-dynamic-async.html#keep-alive-with-Dynamic-Components)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
---
+++ Lesson 26/slot-example/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="showFormOne">Form One</button>
+    <button v-on:click="showFormTwo">Form Two</button>
+  </div>
+</template>
+
+<script>
+// Imports
+import formOne from "./components/FormOne.vue";
+import formTwo from "./components/FormTwo.vue";
+export default {
+  components: {
+    "form-one": formOne,
+    "form-two": formTwo
+  },
+  data() {
+    return {
+      component: "form-one"
+    };
+  },
+  methods: {
+    showFormOne: function() {
+      this.component = "form-one";
+    },
+    showFormTwo: function() {
+      this.component = "form-two";
+    },
+    handleSubmit: function() {
+      alert("thanks for submitting");
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
