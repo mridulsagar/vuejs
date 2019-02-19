40c66b135255dface498460c462ba1908e4f14a6
mridul <mridulsagar699@gmail.com>
2019-02-19 13:14:31 +0600

    Lesson 29: Select Box Binding

--- Lesson 27/my-blog/src/components/AddBlog.vue
+++ Lesson 27/my-blog/src/components/AddBlog.vue
@@ -18,6 +18,10 @@
         <label>Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories">
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{ author }}</option>
+      </select>
     </form>
 
     <div id="preview">
@@ -28,6 +32,8 @@
       <ul>
         <li v-for="category in blog.categories">{{ category }}</li>
       </ul>
+
+      <p>Author: {{ blog.author }}</p>
     </div>
   </div>
 </template>
@@ -39,8 +45,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ["Ninja", "Mario", "Wizards"]
     };
   },
   methods: {}
